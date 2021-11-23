2cf3319a7b5013e4ca1f9a9d7db3af24ed60abc9
Rahul Balhotra <40921516+rahulbalhotra@users.noreply.github.com>
2021-11-23 19:38:59 +0530

    Update job_recommender_app.py

--- job_recommender_app.py
+++ job_recommender_app.py
@@ -88,7 +88,9 @@ st.markdown(match_string)
 
 st.title("Jobs available")
 df = pd.read_csv("naukri_com-job_sample.csv")
-st.subheader("Job title")
+
+df1 = pd.Dataframe(column= 'job title', 'location', 'payrate')
+st.table(df1)
 for ind in range(12):
     k = random.randint(1,1000)
     if 'analy' or 'IT' or 'business' or 'data'  in df['skills'][k]:
